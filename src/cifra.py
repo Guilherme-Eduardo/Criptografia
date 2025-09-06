7959f6d3290d54daad02dae83285f14a0473d39e
Guilherme <guilhermestreet1998@hotmail.com>
2025-09-05 23:16:12 -0300

    UPDATE playfair/Cesar function

--- src/cifra.py
+++ src/cifra.py
@@ -11,6 +11,19 @@ def formatFile():
     return True
 
 
+def timeExecution():
+    return True
+
+# Imprime as informações inicias
+def initialInfo(key, text, matrix, valueLastChar):
+    print (f"\nChave: {key}")
+    print ("Soma do ultimo digito da 1° da linha da tab. ASCII: ", valueLastChar)
+    print (f"Texto Claro: {text}")
+    print ("\nMatriz após a transposicao")
+    printMatrix(matrix)
+    
+    
+
 # Cria a matriz de play fair inicial
 def createPlayfairMatrix(key):
     #Concateno o alfabeto na chave
@@ -19,15 +32,13 @@ def createPlayfairMatrix(key):
     key = "".join(dict.fromkeys(key))
     
     matrix = []
-    # i vai ser 0, 5, 10, 15, 20 -> e incrementa os valores na matriz
+    # i vai ser 0, 5, 10, 15, 20. Incrementa os valores na matriz
     for i in range(0, 25, 5):
         line = []
         for k in key[i:i+5]:
             line.append(k)
         matrix.append(line)
     
-    print("Matriz inicial:")
-    printMatrix(matrix)
     return matrix    
     
     
@@ -47,14 +58,11 @@ def transposition(matrix):
     line = int(index / 5)
     column = index % 5
 
-    print ("\nFazendo a transposição...\n"
-           "Soma do ultimo digito da 1° da linha da tab. ASCII: ", sum_digits)
-    
     #Transposição (Triângulo de Guilherme)
     matrix[0][0] = lineLastChar
     matrix[0][4] = matrix[line][column]
     matrix[line][column] = first  
-    return matrix
+    return index, matrix
     
     
 def printMatrix(matrix):
@@ -62,11 +70,17 @@ def printMatrix(matrix):
         for j in range (0, len(matrix[i])):
             print (matrix[i][j], end=" ")
         print ("")
+        
 
-# Encontra as posições de linha e coluna da respectiva Letra na matriz playfair
+# Encontra as posições de linha e coluna da respectiva letra na matriz playfair
 def findPositionMatrix(matrix, char):
     line = 0
     column = 0
+    
+    # Se o char for 'J, substitui por I
+    char = 'I' if char == 'J' else char
+    
+    #Encontra linha e coluna na matriz
     for i in range (0, len(matrix)):        
         for j in range (0, len(matrix[i])):
             if (char == matrix[i][j]):
@@ -74,64 +88,92 @@ def findPositionMatrix(matrix, char):
                 column = j
                 return line, column
 
-    return 0
-
-
-# Responsavel por fazer as substituções na cifra
-def playfairSubstitutionEnc(matrix, line1, line2, column1, column2):
-    char1 = char2 = 0
-
-    #Letras na mesma linha
-    if line1 == line2:
-        char1 = 0 if column1 % 5 == 1 else column1 + 1
-        char2 = 0 if column1 % 5 == 1 else column2 + 1
-        return matrix[line1][char1], matrix[line2][char2]
     
-    #Letras na mesma coluna
+
+
+# Responsavel por fazer as substituções na cifra e retorna as letras para serem concatenadas
+def playfairSubstitutionEnc(matrix, line1, line2, column1, column2):
+    # mesma linha
+    if line1 == line2:
+        char1 = 0 if column1 == 4 else column1 + 1
+        char2 = 0 if column2 == 4 else column2 + 1
+        return matrix[line1][char1], matrix[line2][char2]
+
+    # mesma coluna
     elif column1 == column2:
-        char1 = 0 if column1 % 5 == 1 else line1 + 1
-        char2 = 0 if column1 % 5 == 1 else line2 + 1
+        char1 = 0 if line1 == 4 else line1 + 1
+        char2 = 0 if line2 == 4 else line2 + 1
         return matrix[char1][column1], matrix[char2][column2]
 
     else:
         return matrix[line1][column2], matrix[line2][column1]
 
-        
-def playfairEncript(matrix, text, key):
+
+
+# Todo o processo de criptografia da playfair
+def playfairEncript(text, key):
+    
     #Cria a matriz e já faz a transposição
     matrix = createPlayfairMatrix(key)
-    matrix = transposition(matrix)
-    encryptedText = []
+    print ("Matriz inicial: ")
+    printMatrix(matrix)
+    valueLastChar, matrix = transposition(matrix)
+    
+    initialInfo(key, text, matrix, valueLastChar)
+    
+    #Variavel que ficará o texto cifrado
+    encryptedText = ""
 
-    #Adiciona a letra "X" caso no final do arquivo seja impar
+    #Adiciona a letra "X", caso o final do arquivo seja impar
     if len(text) % 2 == 1:
         text += 'X'
     
     for i in range (0, len(text), 2):
-        #Pega um par de letras
+        #Pega um par de letras (2 em 2)
         pairLetters = text[i:i+2]
 
         #Localiza as coordenadas na matriz para a respectiva letra/caracter
         line1, column1 = findPositionMatrix (matrix, pairLetters[0])
         line2, column2 = findPositionMatrix (matrix, pairLetters[1])
 
-        #Faz a substituição conforme a cifra de playfair
-        encryptedText.extend(playfairSubstitutionEnc(matrix, line1, line2, column1, column2))
+        #Faz a substituição conforme a cifra de playfair e concatena na string 
+        l1, l2 = playfairSubstitutionEnc(matrix, line1, line2, column1, column2)
+        encryptedText += l1 + l2
+        
+    print ("Texto cifrado com a cifra de PlayFair: ", encryptedText)    
+    return valueLastChar, encryptedText
 
-    print (encryptedText)
+    
     
 def cesarEncrypt(text, value):
-    return True
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    encryptedText = ""
+    
+    for i in range (len(text)):
+        #Pegando o modulo, nunca vamos correr o risco de acessar uma regiao de memoria nao alocada
+        position = (alphabet.index(text[i]) + value + 1) % len(alphabet)
+        encryptedText += alphabet[position]
+            
+    print ("Texto cifrado com a cifra de Cesar: ", encryptedText)
+    return encryptedText
+    
+    
+def encrypt(cleanText, key):
+    value, encryptedTextPF = playfairEncript(cleanText, key)
+    encryptedTextC = cesarEncrypt (encryptedTextPF, value)    
+    print ("resultado final: ", encryptedTextC)   
     
     
 
 def aesEncrypt():
-    return True
-
+    return 0
+    
     
 
 phrase = "CRIPTOGRAFIA"
-playfairEncript(m, phrase, "HELOISA")
+key = "HELOISA"
+
+encrypt (phrase, key)
 
 
     
